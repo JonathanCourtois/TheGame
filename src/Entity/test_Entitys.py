ac42a77693201d98aa94b67058eef3611d3767fe
Jonathan Courtois <courtois.pauc.jonathan@gmail.com>
2025-05-10 15:44:15 +0200

    Add pytest file to test new features !

---
+++ src/Entity/test_Entitys.py
@@ -0,0 +1,76 @@
+import pytest
+from .Entity import Entity
+
+# TheGame/src/Entity/test_Entity.py
+
+def test_entity_initialization():
+    entity = Entity()
+    assert entity.name == "Entity"
+    assert entity.rarity.name == "D"
+    assert entity.constitution == 1
+    assert entity.strength == 1
+    assert entity.focus == 1
+    assert entity.speed == 1
+    assert entity.life == 10
+    assert entity.maxlife == 10
+    assert entity.level == 1
+    assert entity.maxlevel == 20
+    assert entity.cr == entity.calculate_cr()
+    assert entity.gold == 0
+    assert entity.xp == 0
+
+def test_calculate_cr():
+    entity = Entity()
+    expected_cr = int((entity.constitution - 1 + entity.strength - 1 + entity.focus - 1 + entity.speed - 1) / 2 + (entity.maxlife) / 10)
+    assert entity.calculate_cr() == expected_cr
+
+def test_roll_d():
+    entity = Entity()
+    result = entity.roll_d(6)
+    assert 1 <= result <= 6
+
+def test_roll_n_d():
+    entity = Entity()
+    result = entity.roll_n_d(3, 6)
+    assert 3 <= result <= 18
+
+def test_attack():
+    entity = Entity()
+    hit, damage = entity.attack()
+    assert 1 <= hit <= entity.speed
+    assert 1 <= damage <= entity.strength * 2  # Considering critical hit
+
+def test_defend():
+    entity = Entity()
+    initial_life = entity.life
+    entity.defend(hit=5, damage=3)
+    assert entity.life <= initial_life
+
+def test_heal():
+    entity = Entity()
+    entity.maxlife  = 10
+    entity.life     = 5
+    entity.heal(10)
+    assert entity.life == entity.maxlife
+
+def test_rename():
+    entity = Entity()
+    entity.rename("NewName")
+    assert entity.name == "NewName"
+
+def test_check_level():
+    entity = Entity()
+    entity.xp = (5 * entity.level) ** 2
+    entity.Check_level()
+    assert entity.level == 2
+
+def test_gain_xp():
+    entity = Entity()
+    entity.gain_xp((5 * entity.level) ** 2)
+    assert entity.level == 2
+
+def test_upgrade_stats():
+    entity = Entity()
+    initial_constitution = entity.constitution
+    entity.upgrade_stats(credit=1)
+    assert entity.constitution >= initial_constitution
