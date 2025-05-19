a9feba14b0f763d170036d0c4af37fa4e1f176c8
Jonathan Courtois <courtois.pauc.jonathan@gmail.com>
2025-05-19 20:25:05 +0200

    add display sheet class to manage UI

--- src/utils/display.py
+++ src/utils/display.py
@@ -46,3 +46,54 @@ def color_from_rarity(rarity:randgen.Rarity):
         randgen.Rarity.D: Colors.RED,
     }
     return color[rarity]    
+
+class display_sheet:
+    """
+      UNUSED FOR NOW
+    A class to manage a matrix of display cell elements.
+    """
+    def __init__(self, rows:int=7, cols:int=7, cell_size:int=15):
+        self.rows       = rows
+        self.cols       = cols
+        self.cell_size  = cell_size
+        self.sheet      = [["" for _ in range(cols)] for _ in range(rows)]
+
+    def display_cell_map(self):
+        """
+        Display the cell map of the sheet.
+        """
+        [[self.set_cell(row, col, f"{row},{col}", prefix="[", suffix="]", color=Colors.RESET) for col in range(self.cols)] for row in range(self.rows)]
+        self.display()
+        self.clean_sheet()
+
+    def clean_sheet(self):
+        """
+        Clean the sheet.
+        """
+        [[self.set_cell(row, col, f" ", prefix="", suffix="", color=Colors.RESET) for col in range(self.cols)] for row in range(self.rows)]
+
+    def set_cell(self, row:int, col:int, value:str, prefix:str="", suffix:str="", color:Colors=Colors.RESET):
+       """
+       Set a cell in the sheet.
+       """
+       if row < 0 or row >= self.rows or col < 0 or col >= self.cols:
+           raise IndexError("Cell index out of range.")
+       if len(value+prefix+suffix) > self.cell_size:
+           raise ValueError("Value is too long for the cell.")
+       self.sheet[row][col] = f"{prefix}{color}{value:^{self.cell_size-len(prefix)-len(suffix)}}{Colors.RESET}{suffix}"
+
+    def display(self):
+        """
+        Display the entire sheet.
+        """
+        string = ""
+        for row in range(self.rows):
+            for col in range(self.cols):
+                string += f"{self.sheet[row][col]}"
+            string += "\n"
+        print(string)   
+
+if __name__ == "__main__":
+    sheet = display_sheet()
+    sheet.display_cell_map()
+    sheet.display()
